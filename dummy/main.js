2aaca3a445ec655b035b4a20e23c28a5ab60a6eb
easyPro <bl4th4nk@gmail.com>
2017-11-04 23:56:20 +0700

    Add dummy file

---
+++ dummy/main.js
@@ -0,0 +1,24 @@
+/*
+ * DO NOT REMOVE OR MODIFY THIS COPYRIGHT WITHOUT PERMISSION FROM DEVELOPER.
+ *
+ * Created by ePro Dev. Team 04/11/2017.
+ * Copyright 2017 © ePro Dev. Team
+ * All rights reserved.
+ *
+ * All contents in this file is the property of ePro Dev. Team.
+ * You are not allowed to use the file or modify this file, Unless you not remove or modify this copyright.
+ *
+ * email : epro.dev.team@gmail.com
+ */
+
+var md = new MobileDetect(window.navigator.userAgent);
+
+if (md.mobile()) {
+    if (md.phone()) {
+        window.location="https://easypro.github.io/mobile-detect/phone.html";
+    } else if (md.tablet()) {
+        window.location="https://easypro.github.io/mobile-detect/tablet.html";
+    }
+} else {
+    window.location="https://easypro.github.io/mobile-detect/desktop.html";
+}
